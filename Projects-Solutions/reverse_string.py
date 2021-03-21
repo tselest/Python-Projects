3841acc0e0daa3e7adc8ec888e094aae84f0e5d0
tselest <vangelis.tsel-straitouris@outlook.com>
2021-03-21 22:47:48 +0200

    initial commit

--- Projects-Solutions/reverse_string.py
+++ Projects-Solutions/reverse_string.py
@@ -1,24 +1,13 @@
 # Reverse a String - Enter a string and the program will reverse it and print it out.
 
 def val_input(string):
-    if " " in string:
-        for char in string:
-            if char.isdigit():
-                return "NOT A STRING!!"
-
-            elif char.isalpha() | char.isspace():
-                return string[::-1]
-
+    if any(map(str.isdigit, string))==True:
+        return "NOT A STRING!!"
     else:
-        for char in string:
-            if char.isdigit():
-                return "NOT A STRING!!"
-
-            elif char.isalpha() | char.isspace():
-                return string[::-1]
+        return string[::-1]
 
 def reversed():
-    i = input("Give a string:")
+    i = input("Give a string: ")
     print(val_input(i))
 
 if __name__ == '__main__':
